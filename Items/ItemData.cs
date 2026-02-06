c00b6c16522a1140d188780bec21fc2ccb03ddac
IThisGuyHere <cnijdl1@gmail.com>
2026-02-05 21:00:48 -0700

    as it turns out, storing custom data to the save is a lot easier and non-destructive than I thought

--- Items/ItemData.cs
+++ Items/ItemData.cs
@@ -228,30 +228,21 @@ namespace RepoAP
                 APSave.AddStockReceived();
                 APSave.UpdateAvailableItems();
             }
-            else if (itemName.Contains("Upgrade"))
+            else if (itemName.Contains("Upgrade") || itemName.Contains("Item Health Pack"))
             {
-                // To ensure we don't grant upgrades multiple times, we check how many AP upgrades the save file already knows about and compare it to how many we have now.
-                // itemsUpgradesPurchased only tracks non-AP upgrades, which lets the player keep them in addition to the AP ones.
-                int upgradesReceived = StatsManager.instance.itemsPurchasedTotal[itemName] - StatsManager.instance.itemsUpgradesPurchased[itemName];
+                if (!StatsManager.instance.runStats.TryGetValue($"{itemName} ReceivedFromAP", out int upgradesReceived))
+                {
+                    PunManager.instance.SetRunStatSet($"{itemName} ReceivedFromAP", 0);
+                    upgradesReceived = 0;
+                }
                 if (APSave.GetItemsReceived()[itemId] > upgradesReceived)
+                {
                     StatsManager.instance.ItemPurchase(itemName);
+                    PunManager.instance.SetRunStatSet($"{itemName} ReceivedFromAP", upgradesReceived + 1);
+                }
                 else
                     Plugin.Logger.LogDebug("Item " + itemName + " has already been received. Skipping...");
             }
-            else if (itemName.Contains("Health"))
-            {
-                if (!StatsManager.instance.runStats.TryGetValue("healthPacksReceivedFromAP", out int healthPacksReceived))
-                {
-                    PunManager.instance.SetRunStatSet("healthPacksReceivedFromAP", 0);
-                    healthPacksReceived = 0;
-                }
-                if (APSave.GetItemsReceived()[itemId] > healthPacksReceived)
-                {
-                    StatsManager.instance.ItemPurchase(itemName);
-                    PunManager.instance.SetRunStatSet("healthPacksReceivedFromAP", healthPacksReceived + 1);
-                }
-            }
-
 
         }
 
